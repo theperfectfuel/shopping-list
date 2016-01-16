7d755a1921429d99f98f9b229863a5e796028106
theperfectfuel <robert@theperfectfuel.com>
2016-01-16 14:14:27 -0800

    Add functionality working, but there is a bug with the .check-done functionality.

--- js/main.js
+++ js/main.js
@@ -1,24 +1,25 @@
 $(document).ready(function() {
 	//Function for adding new item
-	$( "#shopping-list" ).on("click", ".button", function(event) {
+	$( "body" ).on("click", ".button", function(event) {
     	var newItem = $('#itemAdd').val();
-    	$("<li class='list-item active-item'>" +
-    		"<input type='checkbox' class='check-done'></input>" +
-    		"<span class='list-label'>" + newItem + "</span>" +
-    		"<input type='submit' class='x-mark'></input>" +
-    		"</li>").appendTo("#shopping-list");
-  	});
 
-	// Function for deleting item
-	$(".list-item").on("click", ".x-mark", function(event){
-	//$( ".x-mark" ).mousedown(function() {
-	  $(this).parent(".list-item").remove();
-	});
 
-	//Function for changing to checked item
-	$( ".list-item" ).on("click", ".check-done", function(event){
-	  $(this).parent(".list-item")
-	  .toggleClass("active-item checked-item");
+	    	$("<li class='list-item active-item'>" +
+	    		"<input type='checkbox' class='check-done'></input>" +
+	    		"<span class='list-label'>" + newItem + "</span>" +
+	    		"<input type='submit' class='x-mark'></input>" +
+	    		"</li>").appendTo("#shopping-list");
+	    		// Function for deleting item
+	    		$(".list-item").on("click", ".x-mark", function(event){
+					$(this).parent(".list-item").remove();
+				});
+
+				//Function for changing to checked item
+				$( ".list-item" ).on("click", ".check-done", function(event){
+					$(this).parent(".list-item")
+					.toggleClass("active-item checked-item");
+				});
+
 	});
 
 });
